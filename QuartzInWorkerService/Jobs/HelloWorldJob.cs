0889c31dde82107e4a28cb1a77e8529109558239
Jerry <506670809@qq.com>
2021-01-25 22:13:26 +0800

    添加项目文件。

---
+++ QuartzInWorkerService/Jobs/HelloWorldJob.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuartzInWorkerService.Jobs
+{
+    //[DisallowConcurrentExecution]属性装饰了工作。该属性防止Quartz.NET尝试同时运行同一作业。
+    [DisallowConcurrentExecution]
+    public class HelloWorldJob : IJob
+    {
+        private readonly ILogger<HelloWorldJob> _logger;
+        public HelloWorldJob(ILogger<HelloWorldJob> logger)
+        {
+            _logger = logger;
+        }
+        public Task Execute(IJobExecutionContext context)
+        {
+            _logger.LogInformation("Hello world!");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ QuartzInWorkerService/Program.cs
@@ -0,0 +1,57 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Quartz;
+using QuartzInWorkerService.Extensions;
+using QuartzInWorkerService.Jobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuartzInWorkerService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    //Add the required Quartz.NET services
+                    services.AddQuartz(q =>
+                    {
+                        // Use a Scoped container to create jobs.
+                        //使用作用域创建这个容器
+                        //这告诉Quartz.NET注册一个IJobFactory通过从DI容器中获取创建作业的作业。该Scoped部分意味着您的工作可以使用范围服务，而不仅仅是单例服务或临时服务
+                        q.UseMicrosoftDependencyInjectionScopedJobFactory();
+
+                        #region 未封装的
+                        //// Create a "key" for the job
+                        //var jobKey = new JobKey("HelloWorldJob");
+
+                        //// Register the job with the DI container
+                        //q.AddJob<HelloWorldJob>(opts => opts.WithIdentity(jobKey));
+
+                        //// Create a trigger for the job
+                        //q.AddTrigger(opts =>opts
+                        //    .ForJob(jobKey)
+                        //    .WithIdentity("HelloWorldJob-trigger")
+                        //    .WithCronSchedule("0/5 * * * * ?"));//每5秒钟执行一次 
+                        #endregion
+
+
+                        // Register the job, loading the schedule from configuration 
+                        q.AddJobAndTrigger<HelloWorldJob>(hostContext.Configuration);
+                    });
+                    // Add the Quartz.NET hosted service
+                    //当请求关闭时，此设置可确保Quartz.NET在退出之前等待作业正常结束。
+                    services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
+
+                    services.AddHostedService<Worker>();
+                });
+    }
+}
